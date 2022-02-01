523e865f870ebe23b46dc2832bfe48a6800d50ab
nba-uochman <uchepatric04@gmail.com>
2022-02-01 23:34:25 +0100

    Updates

---
+++ precache-manifest.2439b68a0570f870ac7bdf927e17ca94.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8a4b6a5c3fe9626e5bc63cff378fb7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bebe1c585c1fbdb0b965",
+    "url": "/static/js/2.a8893b9c.chunk.js"
+  },
+  {
+    "revision": "e4bb3b1cba44742c7f31",
+    "url": "/static/js/main.f6fd9daf.chunk.js"
+  },
+  {
+    "revision": "e6ef9f772bdd2d250600",
+    "url": "/static/js/runtime~main.b7ea368b.js"
+  }
+]);
